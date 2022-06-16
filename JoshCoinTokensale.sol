29f4c8be18d67f76411e58dea73735d54b01a819
jerobar <jerobar@pm.me>
2022-06-16 13:31:33 -0300

    JoshCoinTokensale after testing

--- JoshCoinTokensale.sol
+++ JoshCoinTokensale.sol
@@ -7,7 +7,39 @@ import "./JoshCoin.sol";
 
 /**
  * @dev 'Tokensale' implementation of the 'JoshCoin' token.
+ *
+ * Users may mint 1000 tokens per ether until the supply cap of 1,000,000 has been
+ * reached. The contract owner may withdraw funds into their own address.
  */
-contract JoshCoinTokensale {
+contract JoshCoinTokensale is JoshCoin {
+    /**
+     * @dev Withdraw `amount` of contract ether into owner address.
+     */
+    function withdraw(uint256 amount) public onlyOwner {
+        address payable to = payable(msg.sender);
+        to.transfer(amount);
+    }
 
+    /**
+     * @dev Receive fallback function, if called with 1 ether, mints 1,000 tokens
+     * to the caller's address. Function also requires the total supply of tokens
+     * will not exceed 1,000,000.
+     */
+    receive() external payable {
+        uint256 amount = 10**18 * 1_000;
+
+        require(
+            msg.value == 1 ether,
+            "JoshCoinTokensale: Send 1 ether to mint 1,000 tokens"
+        );
+        require(
+            _totalSupply <= (10**18 * 1_000_000) + amount,
+            "JoshCoinTokensale: Token sale has ended"
+        );
+
+        _balances[msg.sender] += amount;
+        _totalSupply += amount;
+
+        emit Transfer(address(0), msg.sender, amount);
+    }
 }
